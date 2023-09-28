7aed33f48d1edf48f5e369eeb64fa76ce721771d
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-09-28 14:57:46 +0900

    firebaseを環境変数に設定

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,21 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCo8QHh-e7rrfVptcDkUt4sZumY0XTTEpM",
-  authDomain: "company-circle-sns-26431.firebaseapp.com",
-  projectId: "company-circle-sns-26431",
-  storageBucket: "company-circle-sns-26431.appspot.com",
-  messagingSenderId: "193772776851",
-  appId: "1:193772776851:web:cdae3e14c7fa099b664c66"
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
 });
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: "AIzaSyCo8QHh-e7rrfVptcDkUt4sZumY0XTTEpM",
+//   authDomain: "company-circle-sns-26431.firebaseapp.com",
+//   projectId: "company-circle-sns-26431",
+//   storageBucket: "company-circle-sns-26431.appspot.com",
+//   messagingSenderId: "193772776851",
+//   appId: "1:193772776851:web:cdae3e14c7fa099b664c66"
+// });
 
 const firestore = firebaseApp.firestore();
 
